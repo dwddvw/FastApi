429d51d2fcfe7d89c461d4609291400b647c4516
dwddvw <davidov.luka.2009@mail.ru>
2026-03-06 18:42:55 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+
+app=FastAPI()
+
+@app.get("/")
+def read_root():
+    html_content="<h1> Hello FastApi</h1>"
+    return HTMLResponse(content=html_content)
